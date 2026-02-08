ce02212f1d64fbf46296be24f8e38fc195c46d4c
Patrick Lam <prof.lam@gmail.com>
2026-02-08 09:34:39 -0500

    version 0 of L11

---
+++ notes/code/L11/mystery_runner.py
@@ -0,0 +1,27 @@
+from typing import Tuple, List, Sequence, Any, Optional
+from expect_error import ExpectError
+from fuzzer import RandomFuzzer, Runner, Outcome
+
+import re
+
+class MysteryRunner(Runner):
+    def run(self, inp: str) -> Tuple[str, Outcome]:
+        x = inp.find(chr(0o17 + 0o31))
+        y = inp.find(chr(0o27 + 0o22))
+        if x >= 0 and y >= 0 and x < y:
+            return (inp, Runner.FAIL)
+        else:
+            return (inp, Runner.PASS)
+
+def fuzz_mystery_runner():
+    mystery = MysteryRunner()
+    random_fuzzer = RandomFuzzer()
+    while True:
+        inp = random_fuzzer.fuzz()
+        result, outcome = mystery.run(inp)
+        if outcome == mystery.FAIL:
+            break
+    print (result)
+
+if __name__ == "__main__":
+    fuzz_mystery_runner()
